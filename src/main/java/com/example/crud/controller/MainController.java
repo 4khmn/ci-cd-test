9e443314edd4c053071b8608d1702aa13d2f252d
AkhminDenis <denis-akhmin@yandex.ru>
2026-03-06 22:50:44 +0300

    my fix3

--- src/main/java/com/example/crud/controller/MainController.java
+++ src/main/java/com/example/crud/controller/MainController.java
@@ -28,6 +28,7 @@ public class MainController {
     @GetMapping("/courses/{id}")
     public CourseDto getCourseById(@PathVariable Long id) {
         log.info("meow");
+        return courseService.getById(id);
     }
 
     @GetMapping("/students")
